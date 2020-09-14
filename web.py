4a4f5dd4972ea89385859e11be37239d25b1ac49
Adit Modi <adit.modi24@gmail.com>
2020-09-14 10:39:16 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 # Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
+
 from flask import Flask, flash, redirect, render_template, request, session, abort
 from random import randint
 
